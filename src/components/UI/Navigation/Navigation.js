dbae1f391225731ac84a2053e98b4110a61d8d2a
PaStar98 <startekp98@gmail.com>
2022-03-17 11:13:44 +0100

    Start

---
+++ src/components/UI/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import styles from './Navigation.module.scss';
+
+const Navigation = () => {
+  return (
+    <header>
+      <nav>Navigation</nav>
+    </header>
+  );
+};
+
+export default Navigation;
