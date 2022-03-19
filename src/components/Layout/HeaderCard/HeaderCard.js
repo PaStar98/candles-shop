9d680bfe08342ee26a773a6749694e53a62cbcb4
PaStar98 <startekp98@gmail.com>
2022-03-19 22:29:23 +0100

    added more configurable header option you can choose which tag to use h1 <-> h5

--- src/components/Layout/HeaderCard/HeaderCard.js
+++ src/components/Layout/HeaderCard/HeaderCard.js
@@ -1,10 +1,21 @@
 import styles from './HeaderCard.module.scss';
 
 const HeaderCard = props => {
+  const { h5, h4, h3, h2, h1, header, text } = props;
+
+  // FIXME: THERE MUST BE BETTER WAY TO DO IT
+  let headerType;
+  if (h5) headerType = <h5>{header}</h5>;
+  else if (h4) headerType = <h4>{header}</h4>;
+  else if (h3) headerType = <h3>{header}</h3>;
+  else if (h2) headerType = <h2>{header}</h2>;
+  else if (h1) headerType = <h1>{header}</h1>;
+  else headerType = <h5>{header}</h5>;
+
   return (
-    <header className={props.className}>
-      <h4>{props.header}</h4>
-      <span>{props.text}</span>
+    <header className={`${styles.header} ${props.className}`}>
+      <h4>{headerType}</h4>
+      <span>{text}</span>
     </header>
   );
 };
