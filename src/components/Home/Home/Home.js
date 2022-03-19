0880a944d7864caf53c700d82c125c50e09f1130
PaStar98 <startekp98@gmail.com>
2022-03-19 22:30:01 +0100

    add header product info on the top of home page

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -2,13 +2,26 @@ import Slider from '../../UI/Slider/Slider';
 import Main from '../../Layout/Main/Main';
 import Headers from './Headers/Headers';
 import ProductsInfoList from './ProductsInfo/ProductsInfoList/ProductsInfoList';
+import HeaderCard from '../../Layout/HeaderCard/HeaderCard';
+import styles from './Home.module.scss';
 
 const Home = () => {
+  const headerCardInfo = {
+    header: 'Jedyne w swoim rodzaju sojowe świece zapachowe!',
+    text: 'Widok pięknych planet na tle galaktyki w połączeniu z niesamowitym zapachem pozwoli Ci na pełen relaks i odpoczynek od codziennych spraw',
+  };
+
   return (
     <Main>
+      <HeaderCard
+        className={styles.header}
+        header={headerCardInfo.header}
+        text={headerCardInfo.text}
+        h2
+      />
+      <ProductsInfoList />
       <Slider />
       <Headers />
-      <ProductsInfoList />
     </Main>
   );
 };
