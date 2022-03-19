b5299261c5b20f7e5ccfdc8b0ae8fd2a9d5d4e35
PaStar98 <startekp98@gmail.com>
2022-03-19 22:27:53 +0100

    clear socials from footer, replaced "DUMMY INFO"

--- src/components/UI/Footer/Footer.js
+++ src/components/UI/Footer/Footer.js
@@ -5,19 +5,7 @@ import styles from './Footer.module.scss';
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <ul className={styles['footer__socials-list']}>
-        <li>
-          <a href="#">Facebook</a>
-        </li>
-
-        <li>
-          <a href="#">Instagram</a>
-        </li>
-
-        <li>
-          <a href="#">Gmail</a>
-        </li>
-      </ul>
+      <span>DUMMY INFO</span>
     </footer>
   );
 };
