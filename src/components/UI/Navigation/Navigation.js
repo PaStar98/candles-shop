290259ab11998e8a5fd53afc8fbc44cce10c31cc
PaStar98 <startekp98@gmail.com>
2022-03-31 17:11:10 +0200

    make nav properly sticky between main content and contact aside

--- src/components/UI/Navigation/Navigation.js
+++ src/components/UI/Navigation/Navigation.js
@@ -1,43 +1,60 @@
-import NavigationItem from './NavigationItem';
-import styles from './Navigation.module.scss';
+import { useState, useEffect } from "react";
+import NavigationItem from "./NavigationItem";
+import styles from "./Navigation.module.scss";
 
-const Navigation = props => {
-  const navigationContent = [
-    {
-      id: 'n1',
-      val: 'Strona Główna',
-    },
-    {
-      id: 'n2',
-      val: 'Świece Sojowe',
-    },
-    {
-      id: 'n3',
-      val: 'O Nas',
-    },
-    {
-      id: 'n4',
-      val: 'Kontakt',
-    },
-  ];
+const NAVIGATION_CONTENT = [
+  {
+    id: "n1",
+    val: "Strona Główna",
+  },
+  {
+    id: "n2",
+    val: "Świece Sojowe",
+  },
+  {
+    id: "n3",
+    val: "O Nas",
+  },
+  {
+    id: "n4",
+    val: "Kontakt",
+  },
+];
 
-  const navigationItems = navigationContent.map(el => (
+const Navigation = (props) => {
+  const [offset, setOffset] = useState();
+
+  useEffect(() => {
+    const onScroll = () => setOffset(window.scrollY);
+
+    window.removeEventListener('scroll', onScroll);
+    window.addEventListener("scroll", onScroll, { passive: true });
+    console.log(offset);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [offset]);
+
+  const navigationItems = NAVIGATION_CONTENT.map((el) => (
     <NavigationItem
       key={el.id}
       id={el.id}
-      className={styles['navigation__list-item']}
+      className={styles["navigation__list-item"]}
       val={el.val}
       onSetPageContent={props.onSetPageContent}
     />
   ));
 
   return (
-    <header className={styles['navigation-container']}>
+    <header
+      className={`${styles["navigation-container"]} ${
+        offset > 30 ? styles.sticky : ""
+      }`}
+    >
       <nav className={styles.navigation}>
-        <h1 className={styles['navigation__logo']}>
+        <h1 className={styles["navigation__logo"]}>
           Candles <span>Shop</span>
         </h1>
-        <ul className={styles['navigation__list']}>{navigationItems}</ul>
+        <ul className={styles["navigation__list"]}>{navigationItems}</ul>
       </nav>
     </header>
   );
